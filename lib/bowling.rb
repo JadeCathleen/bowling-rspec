166683795faa624bcb27c36a948b5fdac1e54f29
JadeCathleen <cormier.jade@gmail.com>
2022-04-05 13:59:47 +0200

    add calculation 1 spare no strike - code not dry

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -9,12 +9,21 @@ class Bowling
   def hit(pin_count)
     @pin_total << pin_count
     if @pin_total.length == 20
-      strike_index = @pin_total.find_index(10)
-      if strike_index.nil?
-        @score = @pin_total.reduce(:+)
-      else
+      strike_index = @pin_total.index(10)
+      pin_total_divided = []
+      for x in (0...20) do
+        pin_total_divided << @pin_total.slice(x, 2) if x.even?
+      end
+      spare_turn = pin_total_divided.select { |turn| turn[0] + turn[1] == 10 }.flatten
+      spare_index = pin_total_divided.index(spare_turn)
+      if strike_index.nil? && spare_index.nil?
+        @score = @pin_total.sum
+      elsif strike_index.nil?
+        @pin_total.push(pin_total_divided[spare_index + 1][0])
+        @score = @pin_total.sum
+      elsif spare_index.nil?
         @pin_total.push(@pin_total[strike_index + 1], @pin_total[strike_index + 2])
-        @score = @pin_total.reduce(:+)
+        @score = @pin_total.sum
       end
     end
   end
