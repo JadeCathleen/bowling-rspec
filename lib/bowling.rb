1c5acb26a990fc98ca6baf5ce57e48529df51015
JadeCathleen <cormier.jade@gmail.com>
2022-04-05 13:15:07 +0200

    improve hit method for score with 1 strike

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -2,10 +2,20 @@ class Bowling
   attr_reader :score
 
   def initialize
+    @pin_total = []
     @score = 0
   end
 
   def hit(pin_count)
-    @score += pin_count
+    @pin_total << pin_count
+    if @pin_total.length == 20
+      strike_index = @pin_total.find_index(10)
+      if strike_index.nil?
+        @score = @pin_total.reduce(:+)
+      else
+        @pin_total.push(@pin_total[strike_index + 1], @pin_total[strike_index + 2])
+        @score = @pin_total.reduce(:+)
+      end
+    end
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe Bowling, "#score" do
       expect(bowling.score).to eq 60
     end
   end
+
+  context "with only one strike" do
+    it "calculates the right score with the strike" do
+      bowling = Bowling.new
+      bowling.hit(10)
+      19.times { bowling.hit(4) }
+      expect(bowling.score).to eq 94
+    end
+  end
 end
