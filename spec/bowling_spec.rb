c139597bf2f79e87d1c4829811b43d233e77e02a
JadeCathleen <cormier.jade@gmail.com>
2022-04-05 12:46:53 +0200

    write first test for bowling score without strike or spares

---
+++ spec/bowling_spec.rb
@@ -0,0 +1,11 @@
+require 'bowling'
+
+Rspec.describe Bowling, "#score" do
+  context "with no strikes or spares" do
+    it "sums up the pin count for each roll" do
+      bowling = Bowling.new
+      20.times { bowling.hit(3) }
+      expect(bowling.score).to eq 60
+    end
+  end
+end
