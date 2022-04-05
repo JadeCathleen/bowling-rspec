1d1bb631ab73651f1458ed027f58f0d6c5e33761
JadeCathleen <cormier.jade@gmail.com>
2022-04-05 14:07:34 +0200

    add a first dry - to be continued

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -10,21 +10,25 @@ class Bowling
     @pin_total << pin_count
     if @pin_total.length == 20
       strike_index = @pin_total.index(10)
-      pin_total_divided = []
-      for x in (0...20) do
-        pin_total_divided << @pin_total.slice(x, 2) if x.even?
-      end
+      pin_total_divided = double_subarray(@pin_total)
       spare_turn = pin_total_divided.select { |turn| turn[0] + turn[1] == 10 }.flatten
       spare_index = pin_total_divided.index(spare_turn)
-      if strike_index.nil? && spare_index.nil?
-        @score = @pin_total.sum
-      elsif strike_index.nil?
+      if strike_index.nil? && spare_index
         @pin_total.push(pin_total_divided[spare_index + 1][0])
-        @score = @pin_total.sum
-      elsif spare_index.nil?
+      elsif spare_index.nil? && strike_index
         @pin_total.push(@pin_total[strike_index + 1], @pin_total[strike_index + 2])
-        @score = @pin_total.sum
       end
+      @score = @pin_total.sum
     end
+  end
+
+  private
+
+  def double_subarray(array)
+    new_array = []
+    for x in (0...20) do
+      new_array << array.slice(x, 2) if x.even?
+    end
+    new_array
   end
 end
